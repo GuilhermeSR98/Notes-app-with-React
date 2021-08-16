11f1e5b04767679097473106e6a8ff8f797171c6
GuilhermeSR98 <guigareisbr@gmail.com>
2021-08-16 09:17:43 -0300

    :art: Calling CSS on Registration Form

--- ceep/src/RegistrationForm/RegistrationForm.jsx
+++ ceep/src/RegistrationForm/RegistrationForm.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-
+import "./style.css";
 class RegistrationForm extends Component {
   render() {
     return (
-      <form>
-        <input type="text" placeholder="title" />
-        <textarea rows={15} placeholder="Write your note" />
-        <button>Create note</button>
+      <form className="registration-form">
+        <input
+          type="text"
+          placeholder="title"
+          className="registration-form_input"
+        />
+        <textarea
+          rows={15}
+          placeholder="Write your note"
+          className="registration-form_input"
+        />
+        <button className="registration-form_input registration-form_submit">
+          Create note
+        </button>
       </form>
     );
   }

30f546b79c1a0d3c06c920291b1f005044930921
GuilhermeSR98 <guigareisbr@gmail.com>
2021-08-18 08:13:53 -0300

    :sparkles: Using SVG Icons

--- ceep/src/components/NoteCard/NoteCard.jsx
+++ ceep/src/components/NoteCard/NoteCard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import deleteSVG from "../../assets/img/delete.svg"
 class NoteCard extends Component {
   render() {
     return (
       <section className="note-card">
         <header className="note-card_header">
           <h3 className="note-card_title">{this.props.title}</h3>
+          <img src={deleteSVG} alt="deleteSVG"/>
         </header>
         <p className="note-card_text">{this.props.text}</p>
       </section>
